c98e44bbc07b55edf4668158764a5bad0856d61c
Santygm04 <santiagogilmina2@gmail.com>
2026-07-11 21:05:51 -0300

    cambiois #4

--- src/app.js
+++ src/app.js
@@ -53,7 +53,7 @@ app.use(cors({
 }));
 
 app.use(helmet());
-app.use(mongoSanitize());
+app.use(mongoSanitize({ onlyAllowDotNotation: true }));
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ extended: true, limit: '2mb' }));
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
